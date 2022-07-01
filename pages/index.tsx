6da2364a48f35d9d7e9e6fabfe4b15ff032726b7
DaryaSsss <d.semina10@gmail.com>
2022-07-01 11:15:08 +0300

    add sort

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { Title, Grid, Center, Select } from '@mantine/core';
+import { Title, Grid, Center, Select, Switch, Group } from '@mantine/core';
+import { useMemo, useState } from 'react';
 import Card from '../components/Card';
 import Layout from '../layouts/Layout';
 
@@ -11,25 +12,52 @@ export interface IPlace {
 }
 
 function Services({ places, errorMsg }: { places?: IPlace[], errorMsg?: string }) {
+  const [placeType, setPlaceType] = useState<string | null>('all');
+  const [checked, setChecked] = useState(false);
+  const filteredPlaces = useMemo(() => {
+    if (!places) return null;
+    const freePlaces = places.filter(place => place.free);
+    if (placeType === 'all') {
+      return freePlaces;
+    }
+      return freePlaces.filter(place => place.place_type === placeType);
+  }, [places, placeType]);
+  const placesToShow = useMemo(() => {
+    if (!filteredPlaces) return null;
+    if (!checked) return filteredPlaces;
+    return [...filteredPlaces].sort((a, b) => a.name.localeCompare(b.name));
+  }, [checked, filteredPlaces]);
   return (
       <Layout>
             <Title order={1} mb="xl">Services</Title>
+            <Group align="center">
             <Select
+              value={placeType}
+              onChange={setPlaceType}
               mb="md"
               style={{ maxWidth: '320px' }}
               label="Choose type of places"
               placeholder="Pick one"
               data={[
+                { value: 'all', label: 'All places' },
                 { value: 'W', label: 'Workplace' },
                 { value: 'O', label: 'Office' },
                 { value: 'MR', label: 'Meeting Room' },
       ]}
             />
-            {places ?
+            <Switch
+              checked={checked}
+              onChange={(event) => setChecked(event.currentTarget.checked)}
+              label="Sort by name"
+              radius="lg"
+              color="dark"
+            />
+            </Group>
+            {placesToShow ?
             <Grid>
-                {places.map(place => (
-                  <Grid.Col span={4}>
-                  <Card place={place} />
+                {placesToShow.map((place, i) => (
+                  <Grid.Col span={4} key={i}>
+                    <Card place={place} />
                   </Grid.Col>
                 ))}
             </Grid>
