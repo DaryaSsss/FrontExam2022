8de813038029acbe0a1e8e16ccfc52b4f340ee00
DaryaSsss <d.semina10@gmail.com>
2022-07-04 19:02:34 +0300

    fix buttons

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,17 @@
-import { Button, Paper, Text } from '@mantine/core';
+import { Button, Paper, Stack, Text } from '@mantine/core';
 import Link from 'next/link';
 import { IPlace } from '../pages';
 
 function Card({ place }: { place: IPlace }) {
     return (
     <Paper shadow="xs" p="md" withBorder style={{ height: '100%' }}>
-        <Text size="xl" mb="md">{place.name}</Text>
+      <Stack style={{ height: '100%' }}>
+        <Text size="xl">{place.name}</Text>
         <Text size="md" mb="md" style={{ whiteSpace: 'break-spaces' }}>{place.desc}</Text>
         <Link href={`places/${place.id}`} passHref>
-            <Button variant="outline" color="dark" component="a" uppercase>Know more</Button>
+            <Button variant="outline" color="dark" component="a" mt="auto" uppercase>Know more</Button>
         </Link>
+      </Stack>
     </Paper>
     );
 }
--- next.config.js
+++ next.config.js
@@ -11,4 +11,5 @@ module.exports = withBundleAnalyzer({
   typescript: {
     ignoreBuildErrors: true,
   },
+  optimizeFonts: false,
 });
